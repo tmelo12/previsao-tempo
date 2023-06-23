ca1f7ceedcef845ff83b1c235e3e1bba64a3252c
Thiago Melo <thiagomelo@super.ufam.edu.br>
2023-06-22 22:22:48 -0400

    all itens config

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+'use client'
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from '@/services/queryClient'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Previsão do Tempo',
+  description: 'Previsão do Tempo',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <QueryClientProvider client={queryClient}>
+
+        <body className={inter.className}>{children}</body>
+      </QueryClientProvider>
+    </html>
+  )
+}
